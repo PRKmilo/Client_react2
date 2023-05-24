b8a9513fd3e9ec2ea4bdf26fff45050efdc169d2
diegobustos329 <dibustosl@unbosque.edu.co>
2023-05-23 21:40:30 -0500

    incorporación de formularios Seg/Profe

---
+++ src/components/forms/Formulario_Prof.js
@@ -0,0 +1,291 @@
+import React, { useState } from 'react';
+import { Form,Button } from 'react-bootstrap';
+
+const FormularioProfesor = () => {
+  const [respuestas, setRespuestas] = useState({
+
+    nombreProfesor: '',
+    asignaturaProfesor: '',
+    departamentoEnsenanza: '',
+    pregunta1_menu1: false,
+    pregunta2_menu1: false,
+    pregunta3_menu1: false,
+    pregunta4_menu1: false,
+    pregunta5_menu1: false,
+    pregunta6_menu1: false,
+    pregunta7_menu1: false,
+    relevanciaCurso: '',
+    cumplimientoObjetivos: '',
+    mejorasCurso: '',
+    quejaReclamoFelicitacion: ''
+  });
+
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    const newValue = type === 'checkbox' ? checked : value;
+    setRespuestas((prevState) => ({ ...prevState, [name]: newValue }));
+
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes enviar las respuestas a través de una API o realizar cualquier acción adicional
+    console.log(respuestas);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group  control="Preguntas_prof">
+        <br/>
+        <h5>
+        Tu opinión es valiosa para nosotros. Agradecemos tu participación en esta encuesta anónima para recopilar reseñas sobre los profesores de nuestra universidad. Tu retroalimentación nos ayudará a evaluar y mejorar la calidad educativa, 
+        así como a proporcionar información útil a futuros estudiantes. 
+        Toda la información proporcionada será tratada de manera confidencial y anónima.
+        </h5>
+        <br/>
+        <div>
+          <label htmlFor="nombreProfesor">Nombre completo del profesor. </label>
+          <select
+            id="nombreProfesor"
+            name="nombreProfesor"
+            value={respuestas.nombreProfesor}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione un profesor</option>
+            <option value="1">profesor 1</option>
+            <option value="2">profesor 2</option>
+            <option value="3">profesor 3</option>
+            <option value="4">profesor 4</option>
+            <option value="5">profesor 5</option>
+          </select>
+        </div>
+        <br/>
+        <div>
+          <label htmlFor="asignaturaProfesor">Curso o Asignatura impartida. </label>
+          <select
+            id="asignaturaProfesor"
+            name="asignaturaProfesor"
+            value={respuestas.asignaturaProfesor}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione una asignatura</option>
+            <option value="1">Asignatura 1</option>
+            <option value="2">Asignatura 2</option>
+            <option value="3">Asignatura 3</option>
+            <option value="4">Asignatura 4</option>
+            <option value="5">Asignatura 5</option>
+          </select>
+        </div>
+        <br/>
+        <div>
+          <label htmlFor="departamentoEnsenanza">Departamento o área de enseñanza. </label>
+          <select
+            id="departamentoEnsenanza"
+            name="departamentoEnsenanza"
+            value={respuestas.departamentoEnsenanza}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione un departamento</option>
+            <option value="1">Departamento 1</option>
+            <option value="2">Departamento 2</option>
+            <option value="3">Departamento 3</option>
+            <option value="4">Departamento 4</option>
+            <option value="5">Departamento 5</option>
+          </select>
+        </div>
+        <br/>
+        <Form.Group  control="Preguntas_menu1">
+          <br/>
+          <h5>Por favor, califica las siguientes áreas del desempeño del profesor en una escala del 1 al 5, donde 1 es "Deficiente" y 5 es "excelente":</h5>
+          <br/>
+          <div>
+          <label htmlFor="pregunta1_menu1">Dominio del contenido del curso.</label>
+          <select
+            id="pregunta1_menu1"
+            name="pregunta1_menu1"
+            value={respuestas.pregunta1_menu1}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione su opinion</option>
+            <option value="1">1 - Deficiente</option>
+            <option value="2">2 - Medio</option>
+            <option value="3">3 - Satisfactorio</option>
+            <option value="4">4 - Muy bueno</option>
+            <option value="5">5 - Excelente</option>
+          </select>
+          </div>
+          <br/>
+          <div>
+          <label htmlFor="pregunta2_menu1">Claridad en la entrega de las clases.</label>
+          <select
+            id="pregunta2_menu1"
+            name="pregunta2_menu1"
+            value={respuestas.pregunta2_menu1}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione su opinion</option>
+            <option value="1">1 - Deficiente</option>
+            <option value="2">2 - Medio</option>
+            <option value="3">3 - Satisfactorio</option>
+            <option value="4">4 - Muy bueno</option>
+            <option value="5">5 - Excelente</option>
+          </select>
+          </div>
+          <br/>
+          <div>
+          <label htmlFor="pregunta3_menu1">Capacidad para mantener el interés de los estudiantes.</label>
+          <select
+            id="pregunta3_menu1"
+            name="pregunta3_menu1"
+            value={respuestas.pregunta3_menu1}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione su opinion</option>
+            <option value="1">1 - Deficiente</option>
+            <option value="2">2 - Medio</option>
+            <option value="3">3 - Satisfactorio</option>
+            <option value="4">4 - Muy bueno</option>
+            <option value="5">5 - Excelente</option>
+          </select>
+          </div>
+          <br/>
+          <div>
+          <label htmlFor="pregunta4_menu1">Habilidad para responder preguntas y dudas.</label>
+          <select
+            id="pregunta4_menu1"
+            name="pregunta4_menu1"
+            value={respuestas.pregunta4_menu1}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione su opinion</option>
+            <option value="1">1 - Deficiente</option>
+            <option value="2">2 - Medio</option>
+            <option value="3">3 - Satisfactorio</option>
+            <option value="4">4 - Muy bueno</option>
+            <option value="5">5 - Excelente</option>
+          </select>
+          </div>
+          <br/>
+          <div>
+          <label htmlFor="pregunta5_menu1">Retroalimentación y evaluación justa de los estudiantes.</label>
+          <select
+            id="pregunta5_menu1"
+            name="pregunta5_menu1"
+            value={respuestas.pregunta5_menu1}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione su opinion</option>
+            <option value="1">1 - Deficiente</option>
+            <option value="2">2 - Medio</option>
+            <option value="3">3 - Satisfactorio</option>
+            <option value="4">4 - Muy bueno</option>
+            <option value="5">5 - Excelente</option>
+          </select>
+          </div>
+          <br/>
+          <div>
+          <label htmlFor="pregunta6_menu1">Disponibilidad para brindar apoyo fuera del horario de clases.</label>
+          <select
+            id="pregunta6_menu1"
+            name="pregunta6_menu1"
+            value={respuestas.pregunta6_menu1}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione su opinion</option>
+            <option value="1">1 - Deficiente</option>
+            <option value="2">2 - Medio</option>
+            <option value="3">3 - Satisfactorio</option>
+            <option value="4">4 - Muy bueno</option>
+            <option value="5">5 - Excelente</option>
+          </select>
+          </div>
+          <br/>
+          <div>
+          <label htmlFor="pregunta7_menu1">Organización y estructura del curso.</label>
+          <select
+            id="pregunta7_menu1"
+            name="pregunta7_menu1"
+            value={respuestas.pregunta7_menu1}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione su opinion</option>
+            <option value="1">1 - Deficiente</option>
+            <option value="2">2 - Medio</option>
+            <option value="3">3 - Satisfactorio</option>
+            <option value="4">4 - Muy bueno</option>
+            <option value="5">5 - Excelente</option>
+          </select>
+          </div>
+        </Form.Group>
+        <Form.Group>
+          <br/>
+          <h5>Responda las preguntas acerca de la relación del docente con el curso.</h5>
+          <br/>
+          <div>
+          <label htmlFor="relevanciaCurso">¿Cómo describirías la relevancia del curso para tus estudios?</label>
+          <select
+            id="relevanciaCurso"
+            name="relevanciaCurso"
+            value={respuestas.relevanciaCurso}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione su opinion</option>
+            <option value="1">1 - Muy poco relevante</option>
+            <option value="2">2 - Poco relevante</option>
+            <option value="3">3 - Relevante</option>
+            <option value="4">4 - Muy relevante</option>
+          </select>
+          </div>
+          <br/>
+          <div>
+          <label htmlFor="cumplimientoObjetivos">¿Consideras que los objetivos del curso se cumplieron satisfactoriamente?</label>
+          <select
+            id="cumplimientoObjetivos"
+            name="cumplimientoObjetivos"
+            value={respuestas.cumplimientoObjetivos}
+            onChange={handleChange}
+          >
+            <option value="">Seleccione su opinion</option>
+            <option value="1">1 - No</option>
+            <option value="2">2 - Muy poco</option>
+            <option value="3">3 - Algo</option>
+            <option value="4">4 - Si</option>
+            <option value="5">5 - Mas de lo necesario</option>
+          </select>
+          </div>
+          <br/>
+          <div>
+          <label htmlFor="mejorasCurso">¿Qué aspectos del curso consideras que podrían mejorarse?</label>
+          <br/>
+          <textarea
+           id="mejorasCurso" 
+           name="mejorasCurso" 
+           value={respuestas.mejorasCurso}
+           onChange={handleChange}
+           rows="4" 
+           cols="50"></textarea>
+          </div>
+          <br/>
+          <div>
+          <label htmlFor="quejaReclamoFelicitacion">¿Alguna queja, reclamo, felicitación o aporte adicional que desee hacer al docente?</label>
+          <br/>
+          <textarea
+           id="quejaReclamoFelicitacion" 
+           name="quejaReclamoFelicitacion" 
+           value={respuestas.quejaReclamoFelicitacion}
+           onChange={handleChange}
+           rows="4" 
+           cols="50"></textarea>
+          </div>
+          
+        </Form.Group>
+      </Form.Group>
+        
+        <div>
+        <Button type="submit">Enviar respuestas</Button>
+        </div>
+    </Form>
+  );
+};
+
+export default FormularioProfesor;
