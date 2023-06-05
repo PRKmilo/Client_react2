cbab788e6c70b4551fcaea5e4ee8f061ec5c3d95
jpizar@unbosque.edu.co <jpizarodrugiez@gmail.com>
2023-06-04 20:40:46 -0500

    agragando mapeo de ruta de update egresado

--- src/helpers/endpoints.js
+++ src/helpers/endpoints.js
@@ -1,5 +1,5 @@
 const API_URL = "http://161.35.9.51";
-export const API_UPDATE_EGRESADOS="/excel_graduado"
+export const API_UPDATE_EGRESADOS="/excel_graduados"
 export const API_USERS="/users";
 export const API_EXCEL_EGRESADOS= "/excel";
 export const  API_URL_STUDENTS = "/students";
